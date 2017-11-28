6271640d77edb0fd0f93e310aa3a02ce312bcca4
juliechiu <jhchiu1@gmail.com>
2017-11-27 21:47:56 -0600

    Added favorite page and button

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var hbs = require('hbs');
+var session = require('express-session');
+var MongoDBStore = require('connect-mongodb-session')(session);
 
 var index = require('./routes/index');
-
+var favorites = require('./routes/favorites');
 var app = express();
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials');
@@ -23,23 +25,43 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var mongo_url = "mongodb://dbUser:password23@ds119306.mlab.com:19306/astropix2"
+
+// Configure session store. Remember to configure DB url for this app's database
+var store = new MongoDBStore({ uri: mongo_url, collection: 'sessions'}, function(err) {
+    if (err) {
+        console.log("Error, can't connect to MongoDB to store sessions");
+    }
+});
+
+// Configure sessions
+app.use(session({
+    secret: 'top secret!',
+    resave: true,
+    saveUninitialized: true,
+    store: store
+}));
+
+
+
 app.use('/', index);
+app.use('/favorites', favorites);     // New favorites route
 
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error');
 });
